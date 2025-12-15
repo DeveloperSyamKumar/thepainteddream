3556ee8a2bed903f23514034e9bcfbcf19c87302
DeveloperSyam <gorlisyam111@gmail.com>
2025-12-15 23:38:36 +0530

    Force Vercel SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { StrictMode } from 'react'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import "./index.css"
-
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
